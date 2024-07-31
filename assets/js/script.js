8eab93664fcd2ec26a46174ceed8323f85961725
ArchiNure <mihrannuredin@gmail.com>
2024-07-31 11:08:49 -0300

    feat: :sparkles: created question data structure and page interaction

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,31 @@ const num1 = 7; // type: float
 const num2 = 2.2; // type: float
 let bool = true; // type: boolean
 let card;
+
+const questions = {
+  q1: {
+    question:
+      "What is the most significant event in the last Harry Potter book?",
+    answers: {
+      a1: "A huge monster caled the Vasilisk",
+      a2: "The battle of Hoguarts",
+      a3: "A monster called the Boggart",
+    },
+  },
+};
+
 const startButton = document.getElementById("run-button");
 const introParagraph = document.getElementById("quiz-intro");
+const questionBox = document.getElementById("create-questions");
+const quizQuestion = document.getElementById("quizQuestion");
+
 startButton.addEventListener("click", function (event) {
   startButton.classList.add("d-none");
   introParagraph.classList.add("d-none");
+
+  quizQuestion.innerText = questions.q1.question;
+
+  questionBox.classList.remove("d-none");
   console.log("Hello!");
 });
 console.dir(startButton);
